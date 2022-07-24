a935389f5e21bcf9619249105bdba8821df574b6
rita- <ritanas00@gmail.com>
2022-07-24 23:25:56 +0300

    helm

--- job2-postgrse-write.py
+++ job2-postgrse-write.py
@@ -6,4 +6,5 @@ with psycopg.connect("postgres://postgres:postgres@postgres-service:5432/bmc-pro
     with conn.cursor() as cur:
 
         cur.execute(f"INSERT INTO users (name) VALUES ('{100 ** 2}')")
+        cur.execute(f"SELECT * FROM users")
         conn.commit()
